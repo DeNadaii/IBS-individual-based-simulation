40f290485a4573b43bdc36b64482239066b893c0
Igor De Nadai <igoroliveiradenadai@gmail.com>
2024-03-19 01:34:28 -0300

    work on object

--- nemVersionObject.py
+++ nemVersionObject.py
@@ -3,126 +3,132 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 import os as OS
 
+class Predador:
+    def __init__(self, genero, idade, posicao):
+        self.genero = genero
+        self.idade = idade
+        self.posicao = posicao
+        
+    def apresentar(self):
+        print("genero: ", self.genero, "\nidade: ", self.idade, "\nposicao: ", self.posicao)
 
-L = 50
+    def crescimentoPredador(self):
+        if self.idade < 4:
+            self.idade += 1
+    def getEndereco(self):
+        return self.posicao
+    def getGenero(self):
+        return self.genero
+    def getIdade(self):
+        return self.idade
+    
+    
+
+L = 10
+
 
 matrix = np.zeros((L, L))
 
-coordenadasPredadores = []
-coordenadasPresas = []
-
+Predadores = []
 
 def gerarPresa():
-    gerar_Presa_x = np.random.randint(0, L-1)
-    gerar_Presa_y = np.random.randint(0, L-1)
- 
-    if matrix[gerar_Presa_y][gerar_Presa_x] != 1:
-        matrix[gerar_Presa_y][gerar_Presa_x] = 1
-        
-    coordenadasPresas.append([gerar_Presa_y,gerar_Presa_x])
-    # print("presa x:", gerar_Presa_x, "presa y:", gerar_Presa_y)
-
-
-def gerarPredador():
-    gerar_Predador_x = np.random.randint(0, L-1)
-    gerar_Predador_y = np.random.randint(0, L-1)
- 
-    if matrix[gerar_Predador_y][gerar_Predador_x] != 2:
-        matrix[gerar_Predador_y][gerar_Predador_x] = 2
+    presa_x = np.random.randint(2,L-2)
+    presa_y = np.random.randint(2,L-2)
+    matrix[presa_x][presa_y] = 1
+    matrix[presa_x][presa_y+1] = 1
+    matrix[presa_x+1][presa_y] = 1
+    matrix[presa_x+1][presa_y+1] = 1  
     
-    coordenadasPredadores.append([gerar_Predador_y,gerar_Predador_x])
-    # print("presa x:", gerar_Presa_x, "presa y:", gerar_Presa_y)
-
+def gerarPredador():
+    predador_x = np.random.randint(0,L-1)
+    predador_y = np.random.randint(0,L-1)
+    matrix[predador_x][predador_y] = 2
+    Predadores.append(Predador("M",4,[predador_x,predador_y]))
 
 def popularMatriz():
     i = 0
     while i < 5:    
-        gerarPresa()   
         gerarPredador() 
+        i += 1
+    i = 0
+    while i <= 1:
+        gerarPresa()
         i += 1
 
 
-print("Predadores: ", coordenadasPredadores)
-print("Presas: ", coordenadasPresas)
+print("Predadores: ", Predadores)
 popularMatriz()
-print("Predadores: ", coordenadasPredadores)
-print("Presas: ", coordenadasPresas)
 
-def checaColisao():
-    count_i = 0 
-    for i in coordenadasPredadores:
-        count_j = 0
-        for j in coordenadasPresas:
-            if i == j:
-                print("engual")
-                print(count_i,coordenadasPredadores)
-                coordenadasPresas.pop(count_j)
-                coordenadasPredadores.insert(count_i,[i[0],i[1]])
-                print(count_i,coordenadasPredadores)
-            count_j += 1
-        count_i += 1
+def gerarLarva(endPredador):
+    print("enderaco da larva", endPredador)
+    matrix[endPredador[0],endPredador[1]] = 2
+    Predadores.append(Predador("M",1,[endPredador[0],endPredador[1]]))
+    
         
 
-def decidirDireçãoGeral(arrEnd, tipo):
+def decidirDireçãoGeral(arrEnd, idade):
     n = np.random.randint(0,2) 
     p = np.random.randint(0,2)
-    # print("p: ", p)
-    # print("n: ", n)
-    matrix[arrEnd[0]][arrEnd[1]] = 0
-    print("antes", arrEnd)
-    if p == 0:
-        delta = (-1)**n
-        arrEnd[1] += delta
-        if arrEnd[1] < 0 or arrEnd[1] > (L-1):
-            print("fora da borda pela pra cima ou pra baixo")
-        if arrEnd[1] > (L-1):
-            arrEnd[1] = 0
-            print("depois EIXO Y", arrEnd)
-        if arrEnd[1] < 0:
-            arrEnd[1] = (L-1)
-            print("depois EIXO Y", arrEnd)
-        if tipo == "presa":
-            matrix[arrEnd[0]][arrEnd[1]] = 1
+    
+    if matrix[arrEnd[0]][arrEnd[1]] == 1:
+        gerarLarva(arrEnd)
+        
+    if idade == 4:
+        print("pode se mover")
+        matrix[arrEnd[0]][arrEnd[1]] = 0
+        if p == 0:
+            delta = (-1)**n    
+            arrEnd[1] += delta
+            if arrEnd[1] < 0 or arrEnd[1] > (L-1):
+                #print("fora da borda pela pra cima ou pra baixo")
+                if arrEnd[1] > (L-1):
+                    arrEnd[1] = 0
+                    #print("depois EIXO Y", arrEnd)
+                if arrEnd[1] < 0:
+                    arrEnd[1] = (L-1)
+                    #print("depois EIXO Y", arrEnd)
+            matrix[arrEnd[0]][arrEnd[1]] = 2
         else:
+            delta = (-1)**n
+            arrEnd[0] += delta
+            if arrEnd[0] < 0 or arrEnd[0] > (L-1):
+                #print("fora da borda pelo esquerda ou direita")
+                if arrEnd[0] > (L-1):
+                    arrEnd[0] = 0
+                    #print("depois EIXO X", arrEnd)
+                if arrEnd[0] < 0:
+                    arrEnd[0] = (L-1)
+                    #print("depois EIXO X", arrEnd)
             matrix[arrEnd[0]][arrEnd[1]] = 2
     else:
-        delta = (-1)**n
-        arrEnd[0] += delta
-        if arrEnd[0] < 0 or arrEnd[0] > (L-1):
-            print("fora da borda pelo esquerda ou direita")
-        if arrEnd[0] > (L-1):
-            arrEnd[0] = 0
-            print("depois EIXO X", arrEnd)
-        if arrEnd[0] < 0:
-            arrEnd[0] = (L-1)
-            print("depois EIXO X", arrEnd)
-        if tipo == "presa":
-            matrix[arrEnd[0]][arrEnd[1]] = 1
-        else:
-            matrix[arrEnd[0]][arrEnd[1]] = 2
-    print("depois", arrEnd)
+        print("nao pode se mover idade baixa")
+        print(arrEnd)
         
-def moverPopulacao(arrEnd,tipo):
-    for i in arrEnd:
-        decidirDireçãoGeral(i,tipo)
-
+def moverPopulacao(ObjetoPredador):
+    for i in ObjetoPredador:
+        decidirDireçãoGeral(i.getEndereco(),i.getIdade())
+        
+for i in Predadores:
+    print(i.getEndereco())
+        
 count = 0
-while count < 100:
-    moverPopulacao(coordenadasPredadores,"predador")
-    moverPopulacao(coordenadasPresas,"presa")
-    checaColisao()
-    cmapmine = ListedColormap(['w','b','r'], N=3)
-    plt.matshow(matrix, cmap=cmapmine, vmin=0, vmax=2)
+while count < 20:
+    moverPopulacao(Predadores)
+    cmapmine = ListedColormap(['w','b','r','g'], N=4)
+    plt.matshow(matrix, cmap=cmapmine, vmin=0, vmax=3)
     plt.title("Frame {}".format(count))
     if count > 9:
         plt.savefig("ResultadoImagens/matrix_IBS_{}.png".format(count), bbox_inches='tight')
     else:
         plt.savefig("ResultadoImagens/matrix_IBS_0{}.png".format(count), bbox_inches='tight')
+    print("count",count)
     count += 1
-    
 
+# for i in Predadores:
+#     print("idade", i.getIdade())
+
+print(matrix)
     
-    
-OS.system("cd ResultadoImagens/ && convert *.png ibs.gif && rm -rf *.png")
+#OS.system("cd ResultadoImagens/ && convert *.png ibs.gif && rm -rf *.png")
 
 
--- newVersion.py
+++ newVersion.py
@@ -10,6 +10,8 @@ matrix = np.zeros((L, L))
 
 coordenadasPredadores = []
 coordenadasPresas = []
+matrizPlotTemporalPredador = []
+matrizPlotTemporalPresa = []
 
 
 def gerarPresa():
@@ -36,7 +38,7 @@ def gerarPredador():
 
 def popularMatriz():
     i = 0
-    while i < 5:    
+    while i < 10:    
         gerarPresa()   
         gerarPredador() 
         i += 1
@@ -69,18 +71,17 @@ def decidirDireçãoGeral(arrEnd, tipo):
     # print("p: ", p)
     # print("n: ", n)
     matrix[arrEnd[0]][arrEnd[1]] = 0
-    print("antes", arrEnd)
     if p == 0:
         delta = (-1)**n
         arrEnd[1] += delta
         if arrEnd[1] < 0 or arrEnd[1] > (L-1):
             print("fora da borda pela pra cima ou pra baixo")
-        if arrEnd[1] > (L-1):
-            arrEnd[1] = 0
-            print("depois EIXO Y", arrEnd)
-        if arrEnd[1] < 0:
-            arrEnd[1] = (L-1)
-            print("depois EIXO Y", arrEnd)
+            if arrEnd[1] > (L-1):
+                arrEnd[1] = 0
+                print("depois EIXO Y", arrEnd)
+            if arrEnd[1] < 0:
+                arrEnd[1] = (L-1)
+                print("depois EIXO Y", arrEnd)
         if tipo == "presa":
             matrix[arrEnd[0]][arrEnd[1]] = 1
         else:
@@ -90,17 +91,16 @@ def decidirDireçãoGeral(arrEnd, tipo):
         arrEnd[0] += delta
         if arrEnd[0] < 0 or arrEnd[0] > (L-1):
             print("fora da borda pelo esquerda ou direita")
-        if arrEnd[0] > (L-1):
-            arrEnd[0] = 0
-            print("depois EIXO X", arrEnd)
-        if arrEnd[0] < 0:
-            arrEnd[0] = (L-1)
-            print("depois EIXO X", arrEnd)
+            if arrEnd[0] > (L-1):
+                arrEnd[0] = 0
+                print("depois EIXO X", arrEnd)
+            if arrEnd[0] < 0:
+                arrEnd[0] = (L-1)
+                print("depois EIXO X", arrEnd)
         if tipo == "presa":
             matrix[arrEnd[0]][arrEnd[1]] = 1
         else:
             matrix[arrEnd[0]][arrEnd[1]] = 2
-    print("depois", arrEnd)
         
 def moverPopulacao(arrEnd,tipo):
     for i in arrEnd:
@@ -111,18 +111,32 @@ while count < 100:
     moverPopulacao(coordenadasPredadores,"predador")
     moverPopulacao(coordenadasPresas,"presa")
     checaColisao()
-    cmapmine = ListedColormap(['w','b','r'], N=3)
-    plt.matshow(matrix, cmap=cmapmine, vmin=0, vmax=2)
-    plt.title("Frame {}".format(count))
-    if count > 9:
-        plt.savefig("ResultadoImagens/matrix_IBS_{}.png".format(count), bbox_inches='tight')
-    else:
-        plt.savefig("ResultadoImagens/matrix_IBS_0{}.png".format(count), bbox_inches='tight')
+    matrizPlotTemporalPredador.insert(count,len(coordenadasPredadores))
+    matrizPlotTemporalPresa.insert(count,len(coordenadasPresas))
+    # cmapmine = ListedColormap(['w','b','r'], N=3)
+    # plt.matshow(matrix, cmap=cmapmine, vmin=0, vmax=2)
+    # plt.title("Frame {}".format(count))
+    # if count > 9:
+    #     plt.savefig("ResultadoImagens/matrix_IBS_{}.png".format(count), bbox_inches='tight')
+    # else:
+    #     plt.savefig("ResultadoImagens/matrix_IBS_0{}.png".format(count), bbox_inches='tight')
     count += 1
+
+def plotTemporalPopulacao():
+    plt.plot(matrizPlotTemporalPresa)
+    plt.ylabel('Presa')
+    plt.xlabel('tempo')
+    plt.savefig("graficoTemporal/Presa", bbox_inches='tight')
+
+    plt.plot(matrizPlotTemporalPredador)
+    plt.ylabel('Predador')
+    plt.savefig("graficoTemporal/Predador", bbox_inches='tight')
     
+
+plotTemporalPopulacao()
 
     
     
-OS.system("cd ResultadoImagens/ && convert *.png ibs.gif && rm -rf *.png")
+#OS.system("cd ResultadoImagens/ && convert *.png ibs.gif && rm -rf *.png")
 
 
